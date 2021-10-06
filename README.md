f56fc452278c25d3ee345993cfd024babe4ccfb4
ZARIF ubuntu <uzbzarif@gmail.com>
2021-10-06 14:58:41 +0900

    fix

--- README.md
+++ README.md
@@ -12,3 +12,12 @@ docker run --rm --runtime=nvidia -v $(pwd):'/github' -it ultralytics/yolov5 bash
 ```
 git clone https://github.com/zylo117/Yet-Another-EfficientDet-Pytorch.git
 ```
+
+
+```
+pip install tensorboardX webcolors
+```
+
+```
+python train.py -c 0 -p logo --head_only False --lr 1e-3 --batch_size 8 --load_weights weights/efficientdet-d0.pth --num_epochs 2 --save_interval 100
+```
--- run_pytorch.sh
+++ run_pytorch.sh
@@ -1,2 +1,6 @@
 #shared memory size either with --ipc=host or --shm-size command line options
 docker run --rm --ipc=host --runtime=nvidia -it -p 8888:8888 -v $(pwd)/Yet-Another-EfficientDet-Pytorch:'/github' ultralytics/yolov5 bash
+
+#docker run --rm --ipc=host --runtime=nvidia --gpus device=0 -it -p 8888:8888 -v $(pwd)/:'/github' ultralytics/yolov5 bash
+#docker run --rm --ipc=host --runtime=nvidia -it -p 8888:8888 -v $(pwd)/:'/github' ultralytics/yolov5 bash
+# python train.py --img 1024 --batch 2 --epochs 2 --data concrete.yml --weights yolov5x6.pt
